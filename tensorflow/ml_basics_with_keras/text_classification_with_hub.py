321277bbc3480c99465699d97bab975e52e251a4
moonwave <moonwave.systems@gmail.com>
2019-11-07 23:34:21 -0800

    text classification

---
+++ tensorflow/ml_basics_with_keras/text_classification_with_hub.py
@@ -0,0 +1,97 @@
+# https://www.tensorflow.org/tutorials/keras/text_classification_with_hub
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+
+import tensorflow as tf
+
+# pip install -q tensorflow-hub
+# pip install -q tensorflow-datasets
+
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+
+print("Version: ", tf.__version__)
+print("Eager mode: ", tf.executing_eagerly())
+print("Hub version: ", hub.__version__)
+print("GPU is", "available" if tf.config.experimental.list_physical_devices("GPU") else "NOT AVAILABLE")
+
+# Download the IMDB dataset
+
+# Split the training set into 60% and 40%, so we'll end up with 15,000 examples
+# for training, 10,000 examples for validation and 25,000 examples for testing.
+train_validation_split = tfds.Split.TRAIN.subsplit([6, 4])
+
+(train_data, validation_data), test_data = tfds.load(
+    name="imdb_reviews", 
+    split=(train_validation_split, tfds.Split.TEST),
+    as_supervised=True)
+
+"""
+Explore the data
+Let's take a moment to understand the format of the data. Each example is a sentence representing the movie review and a corresponding label. The sentence is not preprocessed in any way. The label is an integer value of either 0 or 1, where 0 is a negative review, and 1 is a positive review.
+
+Let's print first 10 examples.
+"""
+train_examples_batch, train_labels_batch = next(iter(train_data.batch(10)))
+train_examples_batch
+
+# Let's also print the first 10 labels.
+
+train_labels_batch
+
+# Build the model
+
+embedding = "https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim/1"
+hub_layer = hub.KerasLayer(embedding, input_shape=[], 
+                           dtype=tf.string, trainable=True)
+hub_layer(train_examples_batch[:3])
+
+# Let's now build the full model:
+
+model = tf.keras.Sequential()
+model.add(hub_layer)
+model.add(tf.keras.layers.Dense(16, activation='relu'))
+model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+
+model.summary()
+
+# Loss function and optimizer
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+
+history = model.fit(train_data.shuffle(10000).batch(512),
+                    epochs=20,
+                    validation_data=validation_data.batch(512),
+                    verbose=1)
+# Evaluate the model
+results = model.evaluate(test_data.batch(512), verbose=2)
+
+for name, value in zip(model.metrics_names, results):
+  print("%s: %.3f" % (name, value))
+
+#@title MIT License
+#
+# Copyright (c) 2017 François Chollet
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
